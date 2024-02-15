b7743fd4ec027dfc52d51a077fa83c81902e1777
silviatommaso <128172559+silviatommaso@users.noreply.github.com>
2024-02-15 12:12:48 +0100

    Create Slime.cs

---
+++ Slime.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slime : MonoBehaviour
+{
+   public float damage = 1;
+   Rigidbody2D rb;
+   public DetectionZone detectionZone;
+   public float moveSpeed = 550f;
+
+
+   void Start(){
+      rb = GetComponent<Rigidbody2D>();
+   }
+
+   
+   void FixedUpdate(){
+      if(detectionZone.detectedObjects.Count > 0){
+         
+         // Calcolo la direzione di movimento
+         Vector2 direction = (detectionZone.detectedObjects[0].transform.position - transform.position).normalized;
+
+         // Mi muovo verso l'obbiettivo
+         rb.AddForce(direction * moveSpeed * Time.deltaTime);
+      }
+   }
+
+   // Lo Slime infligge un danno se entra in collisione
+   void OnCollisionEnter2D(Collision2D col){
+      IDamageable damageable = col.collider.GetComponent<IDamageable>();
+
+      if(damageable != null && col.collider.gameObject.tag == "Player"){
+         damageable.OnHit(damage);
+      }
+   }
+}

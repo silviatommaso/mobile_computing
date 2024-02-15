2af26a0f7b3c7fc6767d02e4700c6227565a050b
silviatommaso <128172559+silviatommaso@users.noreply.github.com>
2024-02-15 14:40:10 +0100

    Delete IDamageable.cs

--- IDamageable.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public interface IDamageable{
-        public float Health{set;get;}
-
-        public void OnHit(float damage, Vector2 knockback);
-        public void OnHit(float damage);
-}
-

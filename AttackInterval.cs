1f47a9be7f1df724010a7aa2d1258e88021fde56
silviatommaso <128172559+silviatommaso@users.noreply.github.com>
2024-02-15 12:40:05 +0100

    Update AttackInterval.cs
    modifica intervallo tra un attacco e l'altro (skill 3)

--- AttackInterval.cs
+++ AttackInterval.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AttackInterval : MonoBehaviour
+public class AttackInterval : MonoBehaviour 
 {
     public DragFingerOffset attackInterval;
 
